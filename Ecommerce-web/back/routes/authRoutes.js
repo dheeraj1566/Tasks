ed5b0c7a10ce405e34c813a09fb506182e179920
dheeraj1566 <dheerajjangid013@gmail.com>
2025-02-25 17:56:05 +0530

    upload on vercel

--- Ecommerce-web/back/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-
-const authRouter = express.Router()
-
-authRouter.get("/check" , (req,res)=>{
-    console.log("cookies" , req.cookies)
-})
-
-export default authRouter

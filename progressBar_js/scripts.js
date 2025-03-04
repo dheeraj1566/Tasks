b295656f9e58361065497eb5d198a2f50f98343b
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-04 16:49:08 +0530

    add text on progressBar

--- progressBar_js/scripts.js
+++ progressBar_js/scripts.js
@@ -16,20 +16,9 @@ btn.addEventListener("click", () => {
 function createBar() {
   const bar = document.createElement("div");
   bar.classList.add("bar");
-  bar.style.position = "relative"; 
-  bar.style.width = "0%";
-  bar.style.backgroundColor = "blue"; 
-  bar.style.height = "30px"; 
-  bar.style.overflow = "hidden"; 
-  bar.style.animation = "progress 4s linear forwards";
   barsDiv.append(bar);
 
   const text = document.createElement("span");
-  text.style.position = "absolute";
-  text.style.left = "50%"; 
-  text.style.transform = "translateX(-50%)";
-  text.style.color = "white";
-  text.style.fontWeight = "bold";
   text.innerText = "0%";
   bar.append(text);
 
@@ -38,7 +27,6 @@ function createBar() {
     progress += 2.56;
     text.innerText = `${Math.round(progress)}%`;
     bar.style.width = `${progress}%`;
-    console.log(progress)
 
     if (progress >= 101) {
       clearInterval(interval);
@@ -53,7 +41,7 @@ function removeBar(bar) {
   bars--;
   if (queue > 0) {
     queue--;
-    bars++; 
+    bars++;
     createBar();
   }
 }

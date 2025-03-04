a158cc47624e721123a5c9639ba7f8a86223d7eb
dheeraj1566 <dheerajjangid013@gmail.com>
2025-03-04 11:09:40 +0530

    resolve error on progressBar

---
+++ progressBar_js/scripts.js
@@ -0,0 +1,34 @@
+const limit = 5;
+const barsDiv = document.querySelector("#bars");
+const btn = document.querySelector("button");
+let bars = 0;
+let queue = 0;
+
+btn.addEventListener("click", () => {
+  if (bars < limit) {
+    bars++;
+    createBar();
+  } else {
+    queue++;
+  }
+});
+
+function createBar() {
+  const bar = document.createElement("div");
+  bar.classList.add("bar");
+  bar.style.animation = "progress 4s linear";
+  barsDiv.append(bar);
+  setTimeout(() => removeBar(bar), 4000);
+}
+
+function removeBar(bar) {
+  bar.remove();
+  bars--;
+  if (queue > 0) {
+    queue--;
+    bars++; 
+    createBar();
+  }
+}
+
+
